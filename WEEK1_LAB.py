d3afdc5ea83babe161588f35149b3e3b70f7c514
Caitlin White <caitlin.vic.white@gmail.com>
2025-10-07 15:43:23 +0100

    Add files via upload

---
+++ WEEK1_LAB.py
@@ -0,0 +1,24 @@
+print("hello, world!")
+
+#storing text (a string)
+student_name = "Caitlin White"
+
+#storing a whole number (an integer)
+student_age = 19
+
+# print the variables to the console 
+print("Student Name:", student_name)
+print("Student Age:", student_age)
+
+#A simple calculation 
+num1 = 15
+num2= 10
+total = num1 + num2
+
+print("the sum is:", total)
+
+Bash:
+
+git add WEEK1_LAB.py
+git commit -m "Feat: Complete Week 1 lab exercises"
+git push
